85de8f36199ae157bbbd754fa63ce0b964ed878b
dberko <dberkowitz@berkpc.com>
2012-11-07 20:28:34 -0500

    Implemented spelling accuracy

--- pytypespeed.py
+++ pytypespeed.py
@@ -1,7 +1,7 @@
 from time import time
 i = 0
 cs = False
-prompt = "A lot of things remind me of my father who passed" ## away when I was 24. Driving on certain back roads he showed me. Stopping at certain stores he always used to go to. Eating at his favorite places. More and more of these places are fading away too though. The roads keep getting built up more and more. The shops and restaurants are closing down. At first it was heart breaking that my father wasn't in this world anymore. But slowly the things that remind me of him are leaving this world as well.A lot of things remind me of my father who passed away when I was 24. Driving on certain back roads he showed me. Stopping at certain stores he always used to go to. Eating at his favorite places. More and more of these places are fading away too though. The roads keep getting built up more and more. The shops and restaurants are closing down. At first it was heart breaking that my father wasn't in this world anymore. But slowly the things that remind me of him are leaving this world as well."
+prompt = "A lot of things remind me of my father who passed away when I was 24. Driving on certain back roads he showed me. Stopping at certain stores he always used to go to. Eating at his favorite places. More and more of these places are fading away too though. The roads keep getting built up more and more. The shops and restaurants are closing down." ##At first it was heart breaking that my father wasn't in this world anymore. But slowly the things that remind me of him are leaving this world as well.A lot of things remind me of my father who passed away when I was 24. Driving on certain back roads he showed me. Stopping at certain stores he always used to go to. Eating at his favorite places. More and more of these places are fading away too though. The roads keep getting built up more and more. The shops and restaurants are closing down. At first it was heart breaking that my father wasn't in this world anymore. But slowly the things that remind me of him are leaving this world as well."
 
 
 def counter():
@@ -9,7 +9,7 @@ def counter():
 	print prompt
 	raw_input(">> Press ENTER to begin")
 	begin_time = time()
-	inp = raw_input()
+	inp = raw_input("\n")
 	end_time = time()
 	final_time = (end_time - begin_time) / 60
 	return final_time, inp
@@ -26,12 +26,22 @@ def wordcheck(inp):
 	prompts = prompt.split()
 	inputs = inp.split()
 	errorcount = 0
-	pairs = zip(inputs, prompts)
-	for pair in pairs:
-		(inputted, accepted) = pair
-		if inputted != accepted:
+	
+	idx = 0
+	for inp in inputs:
+		if inp != prompts[idx]:
 			errorcount += 1
-	return errorcount
+			if inp == prompts[idx + 1]:
+				idx += 2
+			elif inp != prompts[idx - 1]:
+				idx += 1
+		else:
+			idx += 1
+	correct = float(len(prompts)) - float(errorcount)
+	percentage = 100 - ((float(correct) / float(len(prompts))) * 100)
+	
+	return percentage
+
 
 tm, line = counter()
 tm = round(tm, 2)
@@ -39,5 +49,6 @@ words_per_minute = wpm(tm, line)
 words_per_minute = round(words_per_minute, 2)
 print("You total time was: %r minutes")% tm
 print("with an average of: %r words per minute")% words_per_minute
-errorcount = wordcheck(line)
-print("with %r spelling errors") % errorcount
+percentage = wordcheck(line)
+percentager = round(percentage, 2)
+print("with an accuracy of %r %% correct") % percentager
